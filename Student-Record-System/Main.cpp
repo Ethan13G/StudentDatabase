a8c8f58a064bba606e554292b488ac86660ba967
Ethan13G <egee13@student.sl.on.ca>
2026-06-11 02:03:49 -0400

    Updated struct and file format

---
+++ Student-Record-System/Main.cpp
@@ -0,0 +1,91 @@
+// Lab06 - Student Record System (Enhanced)
+// Client Code file - Main.cpp
+// Ethan Gee
+
+#include "Student.h"
+
+using namespace std;
+
+int main()
+{
+	Student students[STUDENT_MAX];
+	int count = 0;
+
+	loadStudents(students, count);
+	calculateAllAverages(students, count);
+
+	int choice;
+
+	do
+	{
+		cout << "\n===== STUDENT DATABASE =====\n";
+		cout << "1. Display\n";
+		cout << "2. Search by Course\n";
+		cout << "3. Assignment Averages\n";
+		cout << "4. Hardest Assignment\n";
+		cout << "5. Course Enrollment\n";
+		cout << "6. Sort by Averages\n";
+		cout << "7. Add Student\n";
+		cout << "8. At Risk Students\n";
+		cout << "9. Save Students\n";
+		cout << "10. Exit\n";
+		cout << endl;
+		cout << "Enter Choice: ";
+		cin >> choice;
+
+		switch (choice)
+		{
+		case 1:
+			displayStudents(students, count);
+			break;
+
+		case 2:
+			searchByCourse(students, count);
+			break;
+		
+
+		case 3:
+			showAssignmentAverage(students, count);
+			break;
+
+		case 4:
+			showHardestAssignment(students, count);
+			break;
+
+		case 5:
+			courseEnrollment(students, count);
+			break;
+
+		case 6:
+			sortByAverage(students, count);
+			displayStudents(students, count);
+			break;
+
+		case 7:
+			addStudent(students, count);
+			saveStudents(students, count);
+			loadStudents(students, count);
+			calculateAllAverages(students, count);
+			displayStudents(students, count);
+			break;
+
+		case 8:
+			atRiskStudents(students, count);
+			break;
+
+		case 9:
+			saveStudents(students, count);
+			break;
+
+		case 10:
+			cout << "\nExiting Program. Goodbye!" << endl;
+			break;
+
+		default:
+			cout << "\nInvalid choice!" << endl;
+		}//end of switch
+
+	} while (choice != 10);
+
+	return 0;
+}//end of main
---
+++ Student-Record-System/Student.cpp
@@ -0,0 +1,79 @@
+// Lab06 - Student Record System (Enhanced)
+// Implementation file - Student.cpp
+// Ethan Gee
+
+#include "Student.h"
+
+using namespace std;
+
+// Load students from file
+void loadStudents(Student students[], int& count)
+{
+	
+}
+
+// ================= DISPLAY =================
+void displayStudents(Student students[], int count)
+{
+    
+}
+
+// Calculate average for a single student
+void calculateAverage(Student* s)
+{
+	
+}
+
+// Calculate averages for all students
+void calculateAllAverages(Student students[], int count)
+{
+
+}
+
+// Search for students taking a specfic course
+void searchByCourse(Student students[], int count)
+{
+
+}
+
+// Show average for each assignment
+void showAssignmentAverage(Student students[], int count)
+{
+
+}
+
+// Show the hardest assignment
+void showHardestAssignment(Student students[], int count)
+{
+
+}
+
+// Show course enrollment numbers
+void courseEnrollment(Student students[], int count)
+{
+
+}
+
+// Sort students by average grade
+void sortByAverage(Student students[], int count)
+{
+
+}
+
+// Add a new student
+void addStudent(Student students[], int& count)
+{
+
+}
+
+// Show at-risk students (at least one assignment below 50 and have an average between 50 and 59)
+void atRiskStudents(Student students[], int count)
+{
+
+}
+
+// Save students to file
+void saveStudents(Student students[], int count)
+{
+
+}
